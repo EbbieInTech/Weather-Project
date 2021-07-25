10410867ce37f3908aded0635f2ab2a7b1f19c8d
Eboni P <eboniperkins@gmail.com>
2021-07-25 19:22:01 -0400

    Updated index.html, style.css, and index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+//display current date and time
+function changeDate(date) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let day = days[date.getDay()];
+  let time = `${now.getHours()}:${
+    (now.getMinutes() < 10 ? "0" : "") + now.getMinutes()
+  }`;
+  let currentDate = `${day} ${time}`;
+
+  return currentDate;
+}
+
 let now = new Date();
+let newdate = document.querySelector("#main-day");
+newdate.innerHTML = changeDate(now);
 
-let date = document.querySelector("#main-day");
+//add a search engine & display name
+function search(event) {
+  event.preventDefault();
 
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+  let currentCity = document.querySelector("#search-text-input");
+  let newCity = document.querySelector("#selector-city");
+  if (currentCity.value) {
+    newCity.innerHTML = `${currentCity.value}`;
+  } else {
+    newCity.innerHTML = null;
+    alert("Please enter a city");
+  }
 }
-
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let day = days[now.getDay()];
-//will return a value between 0 and 6 for the 7 days of the week
-// another example :
-//if (day === 0) {
-//  day = "Sun";}
-
-date.innerHTML = `${day} ${hours}:${minutes}`;
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
